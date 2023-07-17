1e905ffe11187de97a79e9ff376621406e28f3e3
Alejandro Medina <ale_m@outlook.com>
2023-07-17 20:53:20 -0300

    delete import

--- my-portfolio/src/Views/Sitios.jsx
+++ my-portfolio/src/Views/Sitios.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import background from "../assets/baddog/site-singlepage.jpg"
-import bgMulti from "../assets/baddog/site-multipage.jpg"
 import { Link } from 'react-router-dom'
 
 function Sitios() {
